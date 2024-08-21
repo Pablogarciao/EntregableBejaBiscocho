80ec237fd7b210400025fbfc0e472b4dee6b8e53
David Betancur <david.betancur.ramirez.dbr@gmail.com>
2024-08-21 13:21:34 -0500

    Logica de productos

--- src/main/java/com/example/proyectospring/controllers/CuentaController.java
+++ src/main/java/com/example/proyectospring/controllers/CuentaController.java
@@ -1,22 +1,30 @@
 package com.example.proyectospring.controllers;
 
+import com.example.proyectospring.modelentity.Cliente;
 import com.example.proyectospring.modelentity.Cuenta;
+import com.example.proyectospring.modelentity.Producto;
+import com.example.proyectospring.services.ClienteService;
+import com.example.proyectospring.services.Interfaces.IClienteService;
 import com.example.proyectospring.services.Interfaces.ICuentaService;
+import com.example.proyectospring.services.Interfaces.IProductoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController()
 @RequestMapping("/api")
 public class CuentaController {
     @Autowired
     private ICuentaService cuentaService;
+
+    @Autowired
+    private IClienteService clienteService;
+
+    @Autowired
+    private IProductoService productoService;
 
     @GetMapping("/cuenta")
     public List<Cuenta> getCuenta() {
@@ -30,8 +38,36 @@ public class CuentaController {
 
         Map<String,String> response = new HashMap<>();
 
-        try{
+        try {
+            // Verificar que cliente y producto existan
+            Cliente cliente = clienteService.findById(cuenta.getClient().getId());
+            if (cliente == null) {
+                response.put("message", "Client not found");
+                return ResponseEntity.status(404).body(response);
+            }
+
+            Producto producto = productoService.findById(cuenta.getProduct().getId());
+            if (producto == null) {
+                response.put("message", "Product not found");
+                return ResponseEntity.status(404).body(response);
+            }
+
+            // Restar stock
+            int productoQuantity = producto.getQuantity();
+            int cuentaQuantity = cuenta.getQuantity();
+
+            if (productoQuantity < cuentaQuantity) {
+                response.put("message", "Not enough stock");
+                return ResponseEntity.status(404).body(response);
+            }
+
+            producto.setQuantity(productoQuantity - cuentaQuantity);
+            productoService.save(producto);
+
+            // Colocar la cuenta con la fecha actual
             cuenta.setDate(new Date());
+
+            // Guardar la cuenta
             cuentaService.save(cuenta);
         } catch (Exception e){
             response.put("message",e.getMessage());
@@ -65,20 +101,81 @@ public class CuentaController {
     }
 
     @PutMapping("/cuenta/{id}")
-    public Cuenta putCuenta (@RequestBody Cuenta Cuenta, @PathVariable Long id) {
+    public ResponseEntity<?> putCuenta (@RequestBody Cuenta cuenta, @PathVariable Long id) {
         System.out.println("putCuenta");
 
-        Cuenta c= cuentaService.findById(id);
+        Map<String,String> response = new HashMap<>();
 
-        if(c==null){
-            throw new RuntimeException("Cuenta no encontrada");
+        try {
+            Cuenta c = cuentaService.findById(id);
+            if (c == null) {
+                response.put("message", "Cuenta not found");
+                return ResponseEntity.status(404).body(response);
+            }
+
+            // Verificar que cliente y producto existan
+            Cliente cliente = clienteService.findById(cuenta.getClient().getId());
+            if (cliente == null) {
+                response.put("message", "Client not found");
+                return ResponseEntity.status(404).body(response);
+            }
+
+            Producto productoNuevo = productoService.findById(cuenta.getProduct().getId());
+            if (productoNuevo == null) {
+                response.put("message", "Product not found");
+                return ResponseEntity.status(404).body(response);
+            }
+
+            Producto productoAntiguo = c.getProduct();
+
+            // Verificar si son el mismo producto
+            int productoNQuantity = productoNuevo.getQuantity();
+            int productoAQuantity = productoAntiguo.getQuantity();
+            int cuentaAQuantity = c.getQuantity();
+            int cuentaNQuantity = cuenta.getQuantity();
+
+            // Verificar si son el mismo producto
+            if (productoAntiguo.getId() == productoNuevo.getId()) {
+                int diferencia = cuentaAQuantity - cuentaNQuantity;
+
+                productoNuevo.setQuantity(productoNQuantity + diferencia);
+
+                if (productoNuevo.getQuantity() < cuentaNQuantity) {
+                    response.put("message", "Not enough stock");
+                    return ResponseEntity.status(404).body(response);
+                }
+
+                productoService.save(productoNuevo);
+
+            } else {
+                // Devolver el stock
+                productoAntiguo.setQuantity(productoAQuantity + cuentaAQuantity);
+
+                // Restar stock
+                if (productoNQuantity < cuentaNQuantity) {
+                    response.put("message", "Not enough stock");
+                    return ResponseEntity.status(404).body(response);
+                }
+
+                productoNuevo.setQuantity(productoNQuantity - cuentaNQuantity);
+                productoService.save(productoNuevo);
+                productoService.save(productoAntiguo);
+            }
+
+            // Colocar la cuenta con la fecha actual
+            c.setDate(new Date());
+
+            // Guardar la cuenta
+            c.setQuantity(cuenta.getQuantity());
+            c.setProduct(cuenta.getProduct());
+            c.setClient(cuenta.getClient());
+
+            cuentaService.save(c);
+
+            return ResponseEntity.status(201).body(c);
+        } catch (Exception e){
+            response.put("message",e.getMessage());
+            return ResponseEntity.status(500).body(response);
         }
-
-        c.setDate(Cuenta.getDate());
-        c.setQuantity(Cuenta.getQuantity());
-        c.setClient(Cuenta.getClient());
-        c.setProduct(Cuenta.getProduct());
-
-        return cuentaService.save(c);
     }
 }
